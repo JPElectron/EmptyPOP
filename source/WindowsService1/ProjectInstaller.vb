c06b3ba0ab0d97cd3b9796f85511b4ba913c64c0
JPElectron <64099873+JPElectron@users.noreply.github.com>
2020-04-21 18:21:46 -0500

    Add files via upload

---
+++ source/WindowsService1/ProjectInstaller.vb
@@ -0,0 +1,26 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration.Install
+Imports System.ServiceProcess
+Imports Microsoft.Win32
+
+Public Class ProjectInstaller
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+        'After the service is installed, start it
+        AddHandler Me.Committed, Sub()
+                                     Try
+                                         Dim controller As New ServiceController(Me.ServiceInstaller1.ServiceName)
+                                         controller.Start()
+                                     Catch ex As Exception
+                                         'Counldn't start the service for some reason
+                                     End Try
+                                 End Sub
+
+    End Sub
+
+End Class
