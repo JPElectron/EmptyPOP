c4eb20c1eef598b2954e9d99342655f115599c80
JPElectron <64099873+JPElectron@users.noreply.github.com>
2020-04-21 18:17:10 -0500

    Add files via upload

---
+++ source/MailServer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MailServer for EmptyPOP")> 
+<Assembly: AssemblyDescription("www.jpelectron.com")> 
+<Assembly: AssemblyCompany("People Technology Consulting, Inc.")> 
+<Assembly: AssemblyProduct("MailServer for EmptyPOP")> 
+<Assembly: AssemblyCopyright("Copyright © 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4fa136b3-df50-4081-9014-87acbe9a68a4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.2")> 
+<Assembly: AssemblyFileVersion("1.0.0.2")> 
